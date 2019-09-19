c37bdfdbe08a5a77d85f4a9399275a7c3eb110f7
Eli José Carrasquero <ielijose@gmail.com>
2019-09-19 14:16:46 -0500

    Frontend Base

---
+++ resources/js/components/AppComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">
+    <div id="content">
+      <transition name="slide-fade" mode="out-in">
+        <router-view :key="$route.fullPath"></router-view>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
